8fc2e8eafd3fff167d766cae1636409dbf747ec2
Dayhan Henrique <dayhan.santos@gmail.com>
2019-03-24 16:05:44 -0300

    Adicionado o banco H2 e mapeamento da Entity Categoria

--- src/main/java/br/com/dayhan/cursomc/domain/Categoria.java
+++ src/main/java/br/com/dayhan/cursomc/domain/Categoria.java
@@ -1,12 +1,19 @@
 package br.com.dayhan.cursomc.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Categoria implements Serializable {
 
     private static final long serialVersionUID = -590464393390842220L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
