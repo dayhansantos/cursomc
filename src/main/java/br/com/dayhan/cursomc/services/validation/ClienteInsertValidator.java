2ed3208c63d76b44da1829df77c1ad0c89cccff4
Dayhan Henrique <dayhan.santos@gmail.com>
2020-01-30 21:30:31 -0300

    Validação customizada de email não repedido na inserção de Cliente

--- src/main/java/br/com/dayhan/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/dayhan/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.dayhan.cursomc.domain.Cliente;
 import br.com.dayhan.cursomc.domain.enums.TipoCliente;
 import br.com.dayhan.cursomc.dto.ClienteNewDTO;
+import br.com.dayhan.cursomc.repositories.ClienteRepository;
 import br.com.dayhan.cursomc.resources.exception.FieldMessage;
 import br.com.dayhan.cursomc.services.validation.utils.CNP;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 
 	@Override
 	public boolean isValid(ClienteNewDTO value, ConstraintValidatorContext context) {
@@ -19,8 +26,14 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (value.getTipo().equals(TipoCliente.PF.getCod()) && !CNP.isValidCPF(value.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CPF inválido"));
-		} else if (value.getTipo().equals(TipoCliente.PJ.getCod()) && !CNP.isValidCNPJ(value.getCpfCnpj())) {
+		} 
+		if (value.getTipo().equals(TipoCliente.PJ.getCod()) && !CNP.isValidCNPJ(value.getCpfCnpj())) {
 			list.add(new FieldMessage("cpfCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente cliente = clienteRepository.findByEmail(value.getEmail());
+		if(cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
