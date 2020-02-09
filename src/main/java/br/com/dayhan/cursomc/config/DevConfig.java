6ba3a63619e91de1d0727595cdf319acdc108623
Dayhan Henrique <dayhan.santos@gmail.com>
2020-02-08 23:40:38 -0300

    Criando profile de Dev

---
+++ src/main/java/br/com/dayhan/cursomc/config/DevConfig.java
@@ -0,0 +1,27 @@
+package br.com.dayhan.cursomc.config;
+
+import br.com.dayhan.cursomc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instanciateDatabase() throws Exception {
+        if(!strategy.equals("create")) {
+            return false;
+        }
+        return this.dbService.instantiateDatabase() != null;
+    }
+}
